52a72008b83d0f4f17ff5aa6212e69710d9c006c
HctrNvt <169070816+HctrNvt@users.noreply.github.com>
2024-10-14 09:52:45 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,13 +1,27 @@
 from flask import Flask,render_template
 
 app = Flask(__name__)
+# {{ url_for('home') }}
 @app.route('/')
 def home():
-    return render_template("home.html")
+    test()
+    return render_template("Menu.html")
 
-@app.route("/")
+@app.route("/connexion/")
 def index():
-    return render_template("")
+    return render_template("Connexion.html")
+
+@app.route("/animaux/")
+def animaux():
+    return render_template("AjoutAnimaux.html")
+
+@app.route
+def utilisateurs():
+    return render_template("AjoutUtilisateur.html")
+
+def test():
+    print("test")
+
 
 if __name__ == '__main__':
     app.run()
