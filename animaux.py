921e733a98d62a61bb99ee5efd4df2169a0a4edd
HctrNvt <169070816+HctrNvt@users.noreply.github.com>
2024-10-17 11:40:58 +0200

    Ajout de la class Soigneur Grade

--- animaux.py
+++ animaux.py
@@ -1,6 +1,17 @@
 class Animal:
-    def __init__(self,idAnimal,nomAnimal,dateNaissance,dateArrivee):
-        self.infos = [idAnimal,nomAnimal,dateNaissance,dateArrivee]
+    def __init__(self,nomAnimal,dateNaissance,dateArrivee,dateDepart):
+        self.infos = [nomAnimal,dateNaissance,dateArrivee,dateDepart]
+    def getInfos(self):
+        return self.infos
+
+class Soigneur:
+    def __init__(self,nom,prenom,grade:int):
+        self.infos = [nom,prenom,grade]
     def getInfos(self):
         return self.infos
     
+class Grade:
+    def __init__(self,nom,niveau):
+        self.infos = [nom,niveau]
+    def getInfos(self):
+        return self.infos
